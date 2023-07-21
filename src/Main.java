4e8eafafda157ffafe7202bc216c76ee4af642dd
FergusTSmith <FergusTSmith@Gmail.com>
2023-07-21 16:42:02 +0100

    Task Two

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        /*Scanner s = new Scanner(System.in);
         System.out.println("Please enter your first num");
         int firstNum = s.nextInt();
         System.out.println("Please enter your second num");
@@ -33,5 +33,55 @@ public class Main {
         }else{
             System.out.println("Stop");
         }
+        */
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter a number between one and 12");
+        int firstNum = s.nextInt();
+
+        while(firstNum < 1 || firstNum > 12){
+            System.out.println("Error, please enter a number between 1 and 12.");
+            firstNum = s.nextInt();
+        }
+
+        switch(firstNum){
+            case 1:
+                System.out.println("Winter");
+                break;
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+                System.out.println("Spring");
+                break;
+            case 4:
+                System.out.println("Spring");
+                break;
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+                System.out.println("Summer");
+                break;
+            case 7:
+                System.out.println("Summer");
+                break;
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+                System.out.println("Autumn");
+                break;
+            case 10:
+                System.out.println("Autumn");
+                break;
+            case 11:
+                System.out.println("Autumn");
+                break;
+            case 12:
+                System.out.println("Winter");
+                break;
+        }
+
+
     }
 }
